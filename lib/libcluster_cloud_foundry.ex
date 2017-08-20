15ea688dba3d4369d148e54c9cb975a81a52e013
Dylan Griffith <dyl.griffith@gmail.com>
2017-08-20 17:47:17 +1000

    Initial commit: mix new libcluster_cloud_foundry

---
+++ lib/libcluster_cloud_foundry.ex
@@ -0,0 +1,18 @@
+defmodule LibclusterCloudFoundry do
+  @moduledoc """
+  Documentation for LibclusterCloudFoundry.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LibclusterCloudFoundry.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/libcluster_cloud_foundry_test.exs
@@ -0,0 +1,8 @@
+defmodule LibclusterCloudFoundryTest do
+  use ExUnit.Case
+  doctest LibclusterCloudFoundry
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
